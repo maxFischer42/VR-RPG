d0073c468534d32157f6fb686435fd2f36799991
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2019-03-14 13:53:35 -0700

    Maze example for pathfinding and standard assets imported for debugging

---
+++ Assets/EnemyStop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyStop : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<AIPathfinder>())
+        {
+            other.GetComponent<NavMeshAgent>().enabled = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<AIPathfinder>())
+        {
+            other.GetComponent<NavMeshAgent>().enabled = true;
+        }
+    }
+}
---
+++ Assets/ReloadScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadScene : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
