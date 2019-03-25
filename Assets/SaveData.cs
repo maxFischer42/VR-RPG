b6efb257515fa7d67426406f93a7f6bd8e0339df
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2019-03-25 13:11:33 -0700

    Added difficulty selection and data saving

---
+++ Assets/SaveData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class SaveData : MonoBehaviour
+{
+    //0 = easy    1 = medium    2 = hard
+    public int difficulty = 0;
+    public int maxhp;
+    public int currenthp;
+    public float xsave;
+    public float ysave;
+    public float zsave;
+    public string scene;
+
+    public void SetDifficulty(string diff)
+    {
+        switch (diff)
+        { 
+        case "0":
+            difficulty = 0;
+            break;
+        case "1":
+            difficulty = 1;
+            break;
+        case "2":
+            difficulty = 2;
+            break;
+        }
+        GetComponent<SetDifficulty>().loadScene();
+    }
+}
---
+++ Assets/SaveToJson.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveToJson : MonoBehaviour
+{
+    public SaveData dataRef;
+    string dataPath;
+
+    private void Start()
+    {
+        dataPath = Path.Combine(Application.persistentDataPath, "svDTA");
+    }
+
+
+    static void SaveData(SaveData data, string path)
+    {
+        string jsonString = JsonUtility.ToJson(data);
+        using (StreamWriter streamWriter = File.CreateText(path))
+        {
+            streamWriter.Write(jsonString);
+        }
+    }
+
+    static SaveData LoadData (string path)
+    {
+        using (StreamReader streamReader = File.OpenText(path))
+        {
+            string jsonString = streamReader.ReadToEnd();
+            return JsonUtility.FromJson<SaveData>(jsonString);
+        }
+    }
+}
