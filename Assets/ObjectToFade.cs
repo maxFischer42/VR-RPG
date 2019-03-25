7c9da3c590bc7956dbe4fe0a510131a8e2a3093f
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2019-03-25 14:04:44 -0700

    Fade curve

---
+++ Assets/ObjectToFade.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class ObjectToFade
+{
+    public Text textObj;
+    public float length = 4f;
+    public AnimationCurve fadeCurve;
+
+
+}
---
+++ Assets/fadeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fadeManager : MonoBehaviour
+{
+    public ObjectToFade[] ObjectsToFade = new ObjectToFade[1];
+    public float timer;
+
+    public void Update()
+    {
+        timer += Time.deltaTime;
+        for (int i = 0; i < ObjectsToFade.Length; i++)
+        {
+            if (timer <= ObjectsToFade[i].length)
+            {
+                Color newOpacity = ObjectsToFade[i].textObj.color;
+                newOpacity.a = ObjectsToFade[i].fadeCurve.Evaluate(timer / ObjectsToFade[i].length);
+                ObjectsToFade[i].textObj.color = newOpacity;
+            }
+        }
+    }
+}
+
+
