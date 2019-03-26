c73967fe8459c3a2b361f1bf36359f6db778f3e5
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2019-03-26 14:01:05 -0700

    Turbine objective

---
+++ Assets/Rotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    public Material setMat;
+    public Vector3 addition;
+    public GameObject particle;
+    public float mult;
+
+    private void Start()
+    {
+        if(setMat)
+        {
+            GetComponent<Renderer>().material = setMat;
+        }
+        if (particle)
+        {
+            particle.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(addition, Time.deltaTime * mult);
+    }
+}
---
+++ Assets/Turbine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turbine : MonoBehaviour
+{
+    public Rotate[] turbineCogs;
+    public GameObject[] inKeys;
+    public AudioSource aud;
+    public int keys = 3;
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "TurbineKey" && keys > 0)
+        {
+            inKeys[keys - 1].SetActive(true);
+            Destroy(collider.gameObject);
+            keys--;
+            if (keys == 0)
+                ActivateRotation();
+        }
+    }
+
+    void ActivateRotation()
+    {
+        aud.enabled = true;
+        for(int i = 0; i < turbineCogs.Length - 1; i++)
+        {
+            turbineCogs[i].enabled = true;
+        }
+    }
+}
