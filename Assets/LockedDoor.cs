c225d9b53cc59ae23cf89a1b12fe18f1555ade83
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2019-04-19 13:47:43 -0700

    Finalized floor 2 design and gameplay

---
+++ Assets/LockedDoor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LockedDoor : MonoBehaviour
+{
+    public int keysIn = 0;
+    public GameObject[] keyObj;
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "GuardKey")
+        {
+            Destroy(other.gameObject);
+            keysIn += 1;
+            keyObj[keysIn].SetActive(true);
+            if(keysIn == 3)
+            {
+                SceneManager.LoadScene("Opening");
+            }
+        }
+    }
+}
